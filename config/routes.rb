9fcedf3016be2f7613e46318bcd44fa88b916c10
Otto Jonetzki <ottojonetzki@gmail.com>
2019-11-15 15:13:22 +0100

    rake green

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
-    resources :doses, only: [:destroy, :new, :create]
+    resources :doses, only: [:new, :create]
   end
+  resources :doses, only: [:destroy]
 end
